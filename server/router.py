d74cc4031194cc8db7c32fbe3adddda2ebf3f242
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2023-05-12 08:52:22 -0300

    wip router class

---
+++ server/router.py
@@ -0,0 +1,34 @@
+class Router:
+    def __init__(self) -> None:
+        self.__get = []
+        self.__post = []
+        pass
+    
+    def get(self, url, callback):
+        self.__get.append({
+            "url": url,
+            "callback": callback
+        })
+    
+    def post(self, url, callback):
+        self.__post.append({
+            "url": url,
+            "callback": callback
+        })
+
+    def run(self, method, url, payload):
+        if method == 'GET':
+            for route in self.__get:
+                if route['url'] == url:
+                    print('GET',  route['url'])
+                    return route['callback']()
+
+        elif method == 'POST':
+            for route in self.__post:
+                if route['url'] == url:
+                    print('POST', route['url'], payload)
+                    return route['callback'](payload)
+
+        else:
+            print('Method not allowed')
+            return 'Method not allowed'
