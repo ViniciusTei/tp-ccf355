7d0d5592ceee5ee9e14e0d311ccfc601a2c720b8
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2023-05-11 22:49:51 -0300

    add instructions

---
+++ install.ps1
@@ -0,0 +1,5 @@
+python -m venv venv
+
+pip install -r ./requirements.txt
+
+./venv/Scripts/Activate
--- run.ps1
+++ run.ps1
@@ -1,15 +1,1 @@
-param(
-  [Parameter()]
-  $install
-)
-
-Write-Host "Creating environment"
-
-./venv/Scripts/Activate
-
-if($install) {
-  Write-Host "Installing dependecies..."
-  pip install -r ./client/requirements.txt
-}
-
-Write-Host "Everything is ready to use!"
+python ./server/server.py
