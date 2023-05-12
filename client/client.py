34e92e088ba082ace82471d950cc41e14ab4ae97
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2023-05-11 22:23:37 -0300

    read message

--- client/client.py
+++ client/client.py
@@ -1,12 +1,7 @@
 from tkinter import *
 from pages import login
-import socket
 
 if __name__ == '__main__':
-  # server = socket.socket()
-  # port = 4000
-  # server.connect(('localhost', port))
-
   main = Tk()
   main.geometry('750x425')
   main.configure(background="#1C1D2C")
@@ -16,6 +11,4 @@ if __name__ == '__main__':
   loginPage = login.loginPage(main, server=None)
   
   loginPage.start()
-  
-  # server.close()
   
--- server/server.py
+++ server/server.py
@@ -7,10 +7,17 @@ port = 4000
 s.bind(('', port))
 s.listen(5)
 
+routes = ['/user']
+
 print('Server running on port 4000!')
 while True:
     connection, addr = s.accept()
-    connection.send('Connected to server in port 4000'.encode())
+    # connection.send('Connected to server in port 4000'.encode())
     print("Accepted a connection request from %s:%s"%(addr[0], addr[1]))
     data = connection.recv(1024).decode()
-    print(data)
+    dataSplitted = data.split(';')
+    
+    if (dataSplitted[1] == '/user'):
+        connection.send('User connected! Vinicius'.encode())
+    else:
+        connection.send('Route not found!'.encode())
