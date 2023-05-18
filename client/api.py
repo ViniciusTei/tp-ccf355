cadada8ce9b2a89c4d3709a57676aa26738cff1c
@vinicius.teixeira2 <vinicius.teixeira@cbrdoc.com.br>
2023-05-18 14:09:56 -0300

    refactor to send headers in it specifc function

--- client/api.py
+++ client/api.py
@@ -18,40 +18,50 @@ class API:
     def __end_connection(self):
         self.__sock.close()
 
-    def __send(self, msg):
+    def __send(self, msg, headers):
+        responseData = dict()
+        
+        self.__start_connection()
+        
+        self.__sock.send(json.dumps(headers).encode())
+
+        header_response_bytes = self.__sock.recv(4096)
+        
+        header_response = json.loads(header_response_bytes.decode())
+
+        if header_response['status'] == 500:
+            self.__end_connection()
+            return  header_response
         
         self.__sock.send(msg.encode())
 
         # espera sempre um json em formato de string do servidor
         response = self.__sock.recv(4096)
 
-        return json.loads(response.decode())
+        try:
+            responseData = json.loads(response.decode())
+        except ValueError as e:
+            responseData['status'] = 500
+            responseData['message'] = 'Resposta invalida do servidor! Precisa ser uma JSON string!'
+
+        self.__end_connection()
+        
+        return responseData
+
 
     def GET(self, url):
-        self.__start_connection()
-
         header = {
             "method": "GET",
             "content_type": "text",
         }
 
-        header_response = self.__send(json.dumps(header))
-
-        if header_response['status'] == 500:
-            self.__end_connection()
-            return header_response
-
         message = 'GET;' + url
 
-        response = self.__send(message)
-
-        self.__end_connection()
+        response = self.__send(message, headers=header)
         
         return response
 
     def POST(self, url, dictionary):
-        self.__start_connection()
-
         data = json.dumps(dictionary)
 
         header = {
@@ -60,67 +70,56 @@ class API:
             "size": len(data)
         }
 
-        # enviar header com pre-conteudo para server
-        header_response = self.__send(json.dumps(header))
-
-        if header_response['status'] == 500:
-            self.__end_connection()
-            return header_response
-
         # enviar conteudo com url
         message = 'POST;' + url + ';' + data
 
         # espera resposta do servidor para o cliente
-        response = self.__send(message)
-
-        self.__end_connection()
+        response = self.__send(message, headers=header)
 
         return response
     
-    def UPLOAD(self, file):
-        filePath = os.getcwd() + '/client/assets/Logo.png'
-        fileSize = os.path.getsize(filePath)
+    # def UPLOAD(self, file):
+    #     filePath = os.getcwd() + '/client/assets/Logo.png'
+    #     fileSize = os.path.getsize(filePath)
+
+    #     header = {
+    #         "method": "POST",
+    #         "content_type": "file",
+    #         "size": fileSize
+    #     }
+
+    #     # enviar header com pre-conteudo para server
+    #     header_response = self.__send(json.dumps(header))
+
+    #     if header_response['status'] == 500:
+    #         self.__end_connection()
+    #         return header_response
         
-        self.__start_connection()
+    #     payload = {
+    #         'file_name': 'Logo.png'
+    #     }
 
-        header = {
-            "method": "POST",
-            "content_type": "file",
-            "size": fileSize
-        }
+    #     message = 'POST;' + '/upload' + ';' + json.dumps(payload)
 
-        # enviar header com pre-conteudo para server
-        header_response = self.__send(json.dumps(header))
+    #     # espera resposta do servidor para o cliente
+    #     messageResponse = self.__send(message)
 
-        if header_response['status'] == 500:
-            self.__end_connection()
-            return header_response
+    #     with open(filePath, 'rb') as f:
+    #         while True:
+    #             bytes_read = f.read(4096)
+    #             print('sending bytes', len(bytes_read))
+    #             if not bytes_read:
+    #                 break
+    #             self.__sock.sendall(bytes_read)
+
+    #     fileResponseBuff = self.__sock.recv(4096)
         
-        payload = {
-            'file_name': 'Logo.png'
-        }
+    #     fileResponse = json.loads(fileResponseBuff.decode())
 
-        message = 'POST;' + '/upload' + ';' + json.dumps(payload)
+    #     response = self.__send('EOF')
 
-        # espera resposta do servidor para o cliente
-        messageResponse = self.__send(message)
+    #     print(response)
 
-        with open(filePath, 'rb') as f:
-            while True:
-                bytes_read = f.read(4096)
-                print('sending bytes', len(bytes_read))
-                if not bytes_read:
-                    break
-                self.__sock.sendall(bytes_read)
+    #     self.__end_connection()
 
-        fileResponseBuff = self.__sock.recv(4096)
-        
-        fileResponse = json.loads(fileResponseBuff.decode())
-
-        response = self.__send('EOF')
-
-        print(response)
-
-        self.__end_connection()
-
-        return response
+    #     return response
