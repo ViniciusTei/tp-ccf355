0eb5810eb3f64d64346fd17967c94da73e85982f
ViniciusTei <viniciustprates@gmail.com>
2023-05-21 16:30:30 -0300

    create home page

--- client/pages/home.py
+++ client/pages/home.py
@@ -1,8 +1,61 @@
 from tkinter import *
+from PIL import ImageTk, Image
+import os
 
 class HomePage(Frame):
     def  __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
-        headingText = Label(self, text="Home", font="16",bg="#292C3D", fg="#FFFFFF")
-        headingText.pack(side=TOP)
+        buttonSubmit = Button(self, text="Criar lobby", command=self.__handleCreateLobbyButton, bg="#0D9EF1", fg="#FFFFFF", width=12)
+        buttonSubmit.place(x=20, y=20)
+
+        self.__lobiesContainer = Frame(self, width=730, height=300)
+        self.__lobiesContainer.configure(background="#1C1D2C")
+        self.__lobiesContainer.place(x=10, y=50)
+
+        self.__totalLobbies = 0
+
+        # place lobby
+        self.__placeLobby(self.__totalLobbies)
+        self.__placeLobby(self.__totalLobbies)
+        self.__placeLobby(self.__totalLobbies) 
+        
+
+    def __handleCreateLobbyButton(self):
+        print('Criar lobby')
+
+    def __handleEntryLobby(self):
+        print('Entrar lobby')
+
+    def __placeLobby(self, col):
+        lobbyFrame = Frame(self.__lobiesContainer, width=158, height=259)
+        lobbyFrame.configure(background='#292C3D', highlightbackground="white", highlightthickness=1)
+        lobbyFrame.pack_propagate(False)
+        headingText = Label(lobbyFrame, text="Time " + str(col), font="16",bg="#292C3D", fg="#FFFFFF")
+        headingText.pack(side=TOP, pady=10)
+
+        self.__placeUser(lobbyFrame, 0)
+        self.__placeUser(lobbyFrame, 1)
+        self.__placeUser(lobbyFrame, 2)
+        self.__placeUser(lobbyFrame, 3)
+        self.__placeUser(lobbyFrame, 4)
+
+        buttonSubmit = Button(lobbyFrame, text="Entrar", command=self.__handleEntryLobby, bg="#0D9EF1", fg="#FFFFFF")
+        buttonSubmit.pack(side=BOTTOM, pady=10)
+
+        lobbyFrame.grid(row=0, column=col, pady=20, padx=10, ipadx=10, ipady=5)
+
+        self.__totalLobbies+=1
+
+    def __placeUser(self, parent, userIndex):
+        userFrame = Frame(parent, background='#292C3D')
+        image = Image.open(os.getcwd() + '/client/assets/user_images/default.png')
+        image = image.resize((28,28), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        labelImage = Label(userFrame, image=photo, background='#292C3D')
+        labelImage.image=photo
+        labelImage.grid(row=0, column=0)
+        userLabel = Label(userFrame, text="Vinicius", background='#292C3D', fg="#FFFFFF")
+        userLabel.grid(row=0, column=1)
+        userPos = 40 + (userIndex * 35)
+        userFrame.place(x=10, y=userPos)
