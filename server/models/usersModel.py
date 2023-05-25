5b61b92d70db67ce4faa9f2afd1a470562d351d5
buMatheus <matheus.s.alves@ufv.br>
2023-05-24 21:26:05 -0300

    update friend relationship

--- server/models/usersModel.py
+++ server/models/usersModel.py
@@ -1,4 +1,5 @@
 from db import database
+from datetime import datetime
 
 def getAllUsers():
     databaseConn = database.DB().db
@@ -50,7 +51,7 @@ def getUserById(id):
 def updateUser(iduser, username, password , image):
     databaseConn = database.DB().db
 
-    databaseConn.execute('UPDATE user SET image = ? WHERE iduser = ?', (image, iduser,))
+    databaseConn.execute('UPDATE user SET username = ?, password = ?, image = ? WHERE iduser = ?', (username, password, image, iduser,))
     databaseConn.commit()
 
     if iduser == None:
@@ -60,3 +61,49 @@ def updateUser(iduser, username, password , image):
             'id': iduser,
             'image': image
         }
+    
+def createFriendRequisition(iduser1, iduser2):
+    #Status = {Pending, Accept, Refused}
+    now = datetime.utcnow()
+    databaseConn = database.DB().db
+
+    databaseConn.execute('INSERT INTO friend (user_iduser_1,user_iduser_2,start_date,update_date,status) VALUES (?, ?, ?, ?, ?)', (iduser1, iduser2, now, now, 'Pending',))
+    databaseConn.commit()
+    return{
+        'iduser': iduser2
+    }
+
+def updateFriendRequest(iduser1, iduser2, status):
+    friend = getFriendshipByIds(iduser1, iduser2)
+    #Status = {Pending, Accept, Refused}
+    now = datetime.utcnow()
+    databaseConn = database.DB().db
+
+    databaseConn.execute('UPDATE friend SET status = ?, update_date = ? WHERE user_iduser_1 = ? AND user_iduser_2 = ?', (status, now, iduser1, iduser2,))
+    databaseConn.commit()
+
+
+def getFriendshipByIds(iduser1, iduser2):
+    databaseConn = database.DB().db
+
+    usersTupleList = databaseConn.execute('SELECT * FROM friend WHERE user_iduser_1 = ? AND user_iduser_2 = ?', (iduser1, iduser2,)).fetchone()
+    
+    databaseConn.close()
+
+    friendshipDict = {}
+
+    friendshipDict['user_iduser_1'] = usersTupleList[0] 
+    friendshipDict['user_iduser_2'] = usersTupleList[1]
+    friendshipDict['start_date'] = usersTupleList[2]
+    friendshipDict['update_date'] = usersTupleList[3]
+    friendshipDict['status'] = usersTupleList[4]
+
+    print(friendshipDict)
+    return friendshipDict
+
+def deleteFriendshipByIds(iduser1, iduser2):
+    databaseConn = database.DB().db
+
+    databaseConn.execute('DELETE FROM friend WHERE user_iduser_1 = ? AND user_iduser_2 = ?', (iduser1, iduser2,)).fetchone()
+    
+    databaseConn.commit()
