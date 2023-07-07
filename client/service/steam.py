908a376635a99e0400d60a5a3923f7a644116e72
ViniciusTei <viniciustprates@gmail.com>
2023-07-07 18:35:26 -0300

    add handle steam api error

--- client/service/steam.py
+++ client/service/steam.py
@@ -12,7 +12,17 @@ class Steam:
     #    { appid: number, name: string }
     # ] }
     def getAllGamesFromSteam(self):
-        response = requests.get(self.__baseUrl)
-        responseJson = response.json()
-        data = responseJson['applist']['apps']
-        return data
+        
+        try:
+
+            response = requests.get(self.__baseUrl)
+            responseJson = response.json()
+            data = responseJson.get('appList', {}).get('apps', {})
+
+            return data
+        except requests.exceptions.RequestsException as e:
+            print('Erro de requisicao', e)
+        except json.JSONDecodeError as e:
+            print('Erro de dados', e)
+
+        return []
